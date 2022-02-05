6175e31083516bd5d325c82436869de59147b537
Preeti <gopal.preeti@gmail.com>
2022-02-05 16:11:50 +0530

    Cleaned scripts for Okra

--- 3D_Recon/okra/rmseVol.m
+++ 3D_Recon/okra/rmseVol.m
@@ -1,12 +1,5 @@
 function [rel_mseVal,rmseval] = rmseVol(resultVol,testVol)
 
-% meanSqError = mean((resultVol - testVol).^2,3);
-% meanSqError = mean2(meanSqError);
-% msi = mean(testVol.^2,3);
-% msi = mean2(msi);
-% 
-% rmseVal = sqrt(meanSqError)/sqrt(msi);
-
 meanSqError = mean((resultVol(:) - testVol(:)).^2);
 msi = mean(testVol(:).^2);
 
